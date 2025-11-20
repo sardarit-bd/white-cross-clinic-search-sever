1c3e43779902f89002aaccd402d0080863a6e104
Md <sohelf131@gmail.com>
2025-11-20 15:50:17 +0600

    fix cors issue

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000'
+}));
 
 // -----------------------
 // MCP ENDPOINT
