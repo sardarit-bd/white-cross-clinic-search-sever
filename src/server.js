fdc0e1b9dd8f06e494432b16a4c4976470f55ba8
Md Sohel <sohelf131@gmail.com>
2026-03-04 15:31:54 +0600

    test server timeout issues

--- src/server.js
+++ src/server.js
@@ -20,50 +20,29 @@ app.use(cors({
   credentials: true
 }));
 
-app.use(async (req, res, next) => {
-  try {
-    await mongoose.connect(envVars.DB_URL, {
-      serverSelectionTimeoutMS: 30000,
-      connectTimeoutMS: 30000,
-    })
-    console.log('Connected to DB Before API')
-    next();
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      message: 'Database connection failed',
-      error: error.message
-    });
-  }
-});
+// app.use(async (req, res, next) => {
+//   try {
+//     await mongoose.connect(envVars.DB_URL, {
+//       serverSelectionTimeoutMS: 30000,
+//       connectTimeoutMS: 30000,
+//     })
+//     console.log('Connected to DB Before API')
+//     next();
+//   } catch (error) {
+//     res.status(500).json({
+//       success: false,
+//       message: 'Database connection failed',
+//       error: error.message
+//     });
+//   }
+// });
 
 app.use("/api", router);
 
 app.get("/", (req, res) => {
   res.send("Server is running.");
 });
-// -----------------------
-// MCP ENDPOINT
-// -----------------------
-// app.post("/mcp", async (req, res) => {
-//   const response = await handleMCPRequest(req.body);
-//   return res.json(response);
-// });
 
-// -----------------------
-// LOCAL SEARCH ENDPOINT
-// -----------------------
-// app.post("/search", (req, res) => {
-//   const { query } = req.body;
-//   const results = searchAI(query);
-
-//   return res.json({
-//     success: true,
-//     query,
-//     results,
-//     count: results.length,
-//   });
-// });
 app.use(globalErrorHandle);
 
 app.use(notFound);
@@ -73,10 +52,7 @@ app.use(notFound);
 const PORT = 5000;
 const startServer = async () => {
   try {
-    await mongoose.connect(envVars.DB_URL, {
-      serverSelectionTimeoutMS: 30000,
-      connectTimeoutMS: 30000,
-    })
+    await mongoose.connect(envVars.DB_URL)
     console.log('Connected to DB')
   } catch (err) {
     console.log(err)
@@ -88,9 +64,9 @@ const startServer = async () => {
   seedSuperAdmin()
 })()
 
-  // app.listen(PORT, () =>
-  //   console.log(`🔥 Local server running on http://localhost:${PORT}`)
-  // );
+app.listen(PORT, () =>
+  console.log(`🔥 Local server running on http://localhost:${PORT}`)
+);
 
 export default app;
 
